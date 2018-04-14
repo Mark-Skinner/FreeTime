dc86760520ac7451c29d55a3abb561387297a30a
Mark-Skinner <skinz93@optonline.net>
2018-04-14 09:33:37 -0400

    Adding DS directory

---
+++ DataStructures/BinaryTreeNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeTime
+{
+    public class BinaryTreeNode<T> : TreeNode<T>
+    {
+        #region Constructors and Destructor
+
+        public BinaryTreeNode() : base() { }
+        public BinaryTreeNode(T data) : base(data) { }
+        public BinaryTreeNode(T data, BinaryTreeNode<T> Parent) : base(data, Parent) { }
+        public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right) : this(data, null, left, right) { }
+        public BinaryTreeNode(T data, BinaryTreeNode<T> Parent, BinaryTreeNode<T> left, BinaryTreeNode<T> right) : base(data, Parent)
+        {
+            base.Neighbors.AddFirst(right);
+            base.Neighbors.AddFirst(left);
+        }
+
+        ~BinaryTreeNode() { }
+
+        #endregion
+
+        #region Fields
+
+        public BinaryTreeNode<T> Left
+        {
+            get
+            {
+                if (base.Neighbors == null || base.Neighbors.Length == 0)
+                    return null;
+                return (BinaryTreeNode<T>)base.Neighbors[0];
+            }
+            set
+            {
+                if (base.Neighbors == null || base.Neighbors.Length == 0)
+                    base.Neighbors = new CustomLinkedList<TreeNode<T>>(2);
+                base.Neighbors[0] = value;
+            }
+        }
+
+        public BinaryTreeNode<T> Right
+        {
+            get
+            {
+                if (base.Neighbors == null || base.Neighbors.Length == 0)
+                    return null;
+                return (BinaryTreeNode<T>)base.Neighbors[1];
+            }
+            set
+            {
+                if (base.Neighbors == null || base.Neighbors.Length == 0)
+                    base.Neighbors = new CustomLinkedList<TreeNode<T>>(2);
+                base.Neighbors[1] = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DataStructures/Enumerations.cs
@@ -0,0 +1,41 @@
+﻿namespace FreeTime
+{
+    public class Enumerations
+    {
+        public enum PrintTreeMode
+        {
+            /// <summary>
+            /// Print parent, then children.
+            /// </summary>
+            BinaryPrefix,
+            /// <summary>
+            /// Print left child, parent, then right child.
+            /// </summary>
+            BinaryInfix,
+            /// <summary>
+            /// Print children, then parent.
+            /// </summary>
+            BinaryPostfix,
+            /// <summary>
+            /// Print tree is zigzag fashion, starting from left to right.
+            /// </summary>
+            ZigZagStartLeft,
+            /// <summary>
+            /// Print tree is zigzag fashion, starting from right to left.
+            /// </summary>
+            ZigZagStartRight,
+            /// <summary>
+            /// Print tree left to right on each level.
+            /// </summary>
+            LeftToRight,
+            /// <summary>
+            /// Print tree right to left on each level.
+            /// </summary>
+            RightToLeft,
+            /// <summary>
+            /// Prints the entire tree as it is in the structure.
+            /// </summary>
+            Entire
+        }
+    }
+}
